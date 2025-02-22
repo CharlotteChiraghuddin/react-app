efcaffc630d300bcba7401339fb1b39796ccff47
[Charlotte Chiraghuddin] <[charlottechiraghuddinxo@gmail.com]>
2025-02-22 14:56:30 +0000

    The state parameter that is recieved in the redirection URI is compared to the state parameter that was sent in the request.If the states do not match, the authentication process is stopped and an error message is sent to the user

--- src/App.js
+++ src/App.js
@@ -13,20 +13,34 @@ function App() {
   const [results,setResults] = useState([])
   const [request,setRequest]=useState(false);
   const [playlist,setPlaylist]= useState([]);
+  const [state,setState]= useState('');
+  const [authenticate,setAuthenticate]=useState(false);
 
   const onPageLoad = () => {
     HandleRedirect();
     const code = localStorage.getItem("code");
+    const storedState = localStorage.getItem("state");
+    setState(storedState);
     console.log(code);
+    const urlParams = new URLSearchParams(window.location.search);
+    const receivedState = urlParams.get("state");
+    if(authenticate){
+    if (storedState !== receivedState) {
+        // State parameters don't match, reject the request
+        alert("Invalid state parameter. Authentication failed.");
+        return;
+    }
   }
+}
   useEffect(()=>{
   onPageLoad()},[])
-  
+
   const handleSearch = (value)=>{
     setShowResults(true);
     if(value){
     setRequest(value);
     }
+    console.log(state);
   }
   const handleClick =(value)=>{
     console.log(value);
@@ -36,15 +50,15 @@ function App() {
     setPlaylist(prevPlaylist=> prevPlaylist.filter(item=> item!== choice));
   }
   const handleSaveToSpotify = ()=>{
-    AuthenticateUser();
+    AuthenticateUser()
     setPlaylist([]);
+    setAuthenticate(true);
   }
   const handleResults = (value)=>{
     if(value){
     setResults(value);
     }
   }
- 
 
   return (
     <div className="App container">
--- src/AuthenticateUser.js
+++ src/AuthenticateUser.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 
 const client_id = '6961fe134cd64321ab2de3c427d3160d';
 const client_secret = 'abeabc05a98245e684ff5bb8f9e81ebe';
@@ -7,27 +8,23 @@ const authorize = "https://accounts.spotify.com/authorize";
 
     /*Get user authorization*/
     function AuthenticateUser(){
-
+    // Generate a random state parameter
+    const state = Math.random().toString(36).substring(7);
+    
+    localStorage.setItem("state",state);
+        
 
         let url = authorize;
         url += "?client_id=" + client_id;
         url += "&response_type=code";
         url += "&redirect_uri=" + encodeURI(redirect_uri);
+        url += "&state=" + state; 
         url += "&show_dialog=true";
         url += "&scope=user-read-private user-read-email";
+        
 
         
-        /*function handleRedirect(){
-            if(window.location.search.length > 0){
-                const stringQuery = window.location.search;
-                const urlParams = new URLSearchParams(stringQuery);
-                const newCode = urlParams.get("code");
-                if(newCode){
-                    setCode(newCode);
-                    localStorage.setItem("code",code);
-                }
-            }
-        }*/
+
 
         window.location.href = url; // Show Spotify's authorization screen
 }
